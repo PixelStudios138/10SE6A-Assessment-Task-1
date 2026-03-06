4afb5934c73cd7f9b863298f7f9cbc02647a9456
PixelStudios138 <marleybroadbent@gmail.com>
2026-03-06 15:08:01 +1100

    chore: did more practice
    Learnt more about the data I could use for the assessment

--- weather_practice.py
+++ weather_practice.py
@@ -12,10 +12,15 @@ def send_request(city):
 def display_weather_info(data):
     if data:
         location = data["location"]["name"]
-        region = data["llocation"]["region"]
+        region = data["location"]["region"]
         country = data["location"]["country"]
-        temperature = data["current"["temp_c"]]
-        print(location, region, country, temperature)
+        temperature = data["current"]["temp_c"]
+        condition = data["current"]["condition"]["text"]
+        condition = condition.lower()
+        print(f"The temperature in {location}, {region}, {country} is {temperature}°C")
+        print(f"It is {condition}")
 
 city_to_analyse = input("What city would you like to view data on?")
 weather_data = send_request(city_to_analyse)
+print(weather_data)
+#display_weather_info(weather_data)
