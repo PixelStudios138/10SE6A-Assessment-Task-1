57cf5aa3d6c6fa808a52e58770f749f3e66cd349
PixelStudios138 <marleybroadbent@gmail.com>
2026-03-16 10:13:57 +1100

    chore: added doc strings to all functions and added timeout feature to api requests

--- functions.py
+++ functions.py
@@ -5,19 +5,33 @@ base_url = "https://api.weatherapi.com/v1"
 api_type = "" # String to determine what type of data to request, e.g. general weather, forecasts, alerts, etc.
 
 def send_request(city, api_type):
+    """Send requests to the API and get data. The url variable combines all necessary features
+       for the request to work. It will try to get data, with 3 exceptions. The first is if it
+       takes too long to connect to the API and it times out. The second is if the API takes
+       too long to return data, again returning a time out error. The final exception is if the
+       api connects, but returns an error code. If none of these exceptions are raised, the api
+       will return the database to be stored in a variable and be used later in the program."""
     url = f"{base_url}/{api_type}?key={api_key}&q={city}"
-    response = requests.get(url)
-    try: 
-        if response.status_code == 200:
-            return response.json()
-        else:
-            print(f"Failed to get weather data. Status code: {response.status_code}")
+    try:
+        response = requests.get(url, timeout=(3, 3))
+        try: 
+            if response.status_code == 200:
+                return response.json()
+            else:
+                print(f"Failed to get weather data. Status code: {response.status_code}")
+                return None
+        except:
+            print("An error occurred while retrieving weather data.")
             return None
-    except:
-        print("An error occurred while retrieving weather data.")
-        return None
+    except requests.exceptions.ConnectTimeout as e:
+        print(f"Connection to API timed out. Try again later, ({e})")
+    except requests.exceptions.ReadTimeout as e2:
+        print(f"Data sent to API timed out: {e2}")
 
 def gather_main_data(data):
+    """This function gathers basic weather data, which will be immediately displayed to the user.
+       The data that will be displayed is the temperature, condition, and what the temperature feels
+       like for the given location."""
     if data:
         location = data["location"]["name"]
         region = data["location"]["region"]
@@ -29,6 +43,7 @@ def gather_main_data(data):
         print(f"It is {condition}")
 
 def get_wind_data(data):
+    """Presents wind data, such as speed, direction, chill, and gust data"""
     if data:
         wind_speed = data["current"]["wind_kph"]
         wind_direction = data["current"]["wind_dir"]
@@ -38,6 +53,7 @@ def get_wind_data(data):
         print(f"There is a gust of {gust}kph")
 
 def get_rain_data(data):
+    """Gets data related to rain, such as precipitation, cloud cover, and dew point."""
     if data:
         precipitation = data["current"]["precip_mm"]
         cloud = data["current"]["cloud"]
@@ -47,6 +63,8 @@ def get_rain_data(data):
         print(f"The dew point is {dew_point}°C")
 
 def get_extra_data(data):
+    """This function gets additional information that the user may wish to view, such as humidity,
+       pressure, visibility, and uv index and presents it to the user"""
     if data:
         humidity = data["current"]["humidity"]
         pressure = data["current"]["pressure_mb"]
@@ -55,11 +73,12 @@ def get_extra_data(data):
         print(f"The pressure is {pressure}mb")
         print(f"The humidity is {humidity}%")
         print(f"The UV index is {uv_index}")
+        print(f"The visibility is {visibility}km")
 
 def temp_ui():
     """Temporary UI to test functions. This will eventually be replaced with a tkinter app-interface.
-       This will prompt the user to choose the type of data they wish to view.
-       This will simulate how the final app will work, but in the terminal instead of a graphical interface."""
+        This will prompt the user to choose the type of data they wish to view.
+        This will simulate how the final app will work, but in the terminal instead of a graphical interface."""
     city_to_analyse = input("What city would you like to view data on? ").lower()
     weather_data = send_request(city_to_analyse, "current.json")
     gather_main_data(weather_data)
